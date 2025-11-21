4a86ec98c9802842458d00eeafbba91166ed6fe6
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-11-21 08:41:05 +0100

    refactor: cluster node class
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

--- cluster.py
+++ cluster.py
@@ -5,15 +5,19 @@ class Cluster:
         self.membership_list = set()
         self.mutex = threading.Lock()
 
-    def add_node(self):
+    def add_node(self, node):
         pass
 
-    def delete_node(self):
+    def delete_node(self, nodeId):
         pass
 
     def fetch_membership_list(self):
         return self.membership_list
 
 class Node:
-    def __init__(self):
-        pass
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+
+    def get_fields(self):
+        return [self.name, self.address]
