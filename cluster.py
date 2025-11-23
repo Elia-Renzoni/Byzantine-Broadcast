46cb1bef3b222e77a75f3d0995d91c762e225ae3
Elia-Renzoni <elia.renzoni03@gmail.com>
2025-11-23 15:00:55 +0100

    chore: added new items
    Signed-off-by: Elia-Renzoni <elia.renzoni03@gmail.com>

--- cluster.py
+++ cluster.py
@@ -6,10 +6,30 @@ class Cluster:
         self.mutex = threading.Lock()
 
     def add_node(self, node):
-        pass
+        try:
+            self.mutex.acquire()
+            self.membership_list.add(node)
+            return True
+        except:
+            return False
+        finally:
+            self.mutex.release()
 
-    def delete_node(self, nodeId):
-        pass
+    def delete_node(self, node):
+        try:
+            self.mutex.acquire()
+            toDelete = None
+            for peer in self.membership_list:
+                _, addr = peer.get_fields()
+                if addr == node:
+                    toDelete = peer
+                    break
+            self.membership_list.remove(toDelete)
+            return True
+        except:
+            return False
+        finally:
+            self.mutex.release()
 
     def fetch_membership_list(self):
         return self.membership_list
--- handlers.py
+++ handlers.py
@@ -1,14 +1,47 @@
 import conn as cn
+import cluster as group
+
+pgroup = group.Cluster()
 
 # match the handler based on req id
 def request_strainer(req):
+    corruption_status = compute_checksum(req['checksum'])
+    if corruption_status is True:
+        return False, "error detected while performing checksum"
+
+    result, message = None, None
+    match req.get_request_id():
+        case "/join":
+            result, message = handle_node_join(req)
+        case "/msg":
+            pass
+        case "/propose":
+            pass
+        case "/health":
+            pass
+        case "/replication":
+            pass
+        case _:
+            pass
+
+    return result, message
+
+def handle_client_message(req):
     pass
 
-def handle_client_message(req, res):
+def handle_replication_message(req):
     pass
 
-def handle_replication_message(req, res):
+def handle_health_check(req):
     pass
 
-def handle_health_check(req, res):
+def handle_node_join(req):
+    if req['body'] is None:
+        return False, "empty data"
+    
+    if pgroup.add_node(req['body']) is True:
+        return True, "join completed"
+    return False, "join aborted"
+
+def compute_checksum(checksum):
     pass
